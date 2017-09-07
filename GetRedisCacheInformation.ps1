3a4d4f20e411145dd22abd9686e379dd88c8f401
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-09-07 23:31:48 +0530

    Added block thoughts around the flow

--- GetRedisCacheInformation.ps1
+++ GetRedisCacheInformation.ps1
@@ -1,7 +1,45 @@
-Get-Module azure | format-table version
+# Introduction
+
+Write-Host '...Hi...'
+Write-Host 'You are using Redis Cache Json Generator'
+Write-Host 'By using this script, you are not agreeing to any liability or licences :)'
+Write-Host 'This script may install a few powershell modules to work, mostly around Azure CLI.'
+
+# Should we uninstall the modules 
+y
+
+if ($UninstallModules -eq 'Y' -or $UninstallModules -eq 'y') {
+    set-variable -name UninstallModules -value TRUE
+}
+else {
+    set-variable -name UninstallModules -value FALSE
+}
+
+# Check if PowerShellGet is installed
+$IsPowerShellGetInstalled = Get-Module PowerShellGet -list | Select-Object Name,Version,Path
+
+# Give instructions to install powershellget
+# https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-4.3.1#how-to-get-powershellget
+
+
+# Check if Azure Rm is available
+
+# Install Azure Rm if unavailable
+
+# Login to Azure RM account
 Login-AzureRmAccount
+
+# Get Azure subscriptions on this account
 Get-AzureRmSubscription | sort SubscriptionName | Select SubscriptionName
+
+# Provide option to select a subscription
+# It would be better if the user can select a number
 Select-AzureRmSubscription -SubscriptionName ContosoSubscription
+
+# Get Redis account details
 Get-AzureRmRedisCache
+
+# Iterate through the cahces to get key
 Get-AzureRmRedisCacheKey -Name myCache -ResourceGroupName myGroup
-# Put in json
+
+# Save in the JSOn format supported by https://github.com/uglide/RedisDesktopManager/
